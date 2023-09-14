48208059919cd5bc9c40464a8945607cc877702b
RbenMoise <reubenmoses91@gmail.com>
2023-09-14 01:01:18 -0700

    betty

--- main.c
+++ main.c
@@ -1,60 +1,98 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "main.h"
 
-#define MAX_INPUT_SIZE 1024
-
-int main() {
+/**
+ * main - Entry point for the simple shell program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
     char input[MAX_INPUT_SIZE];
     int should_run = 1;
-    pid_t pid; /* Move the declaration here */
 
-    while (should_run) {
-        printf("#cisfun$ "); /* Display the prompt */
-        fflush(stdout); /* Make sure the prompt is displayed */
+    while (should_run)
+    {
+        printf("Shell > ");
+        fflush(stdout);
 
         /* Read user input */
-        if (fgets(input, sizeof(input), stdin) == NULL) {
-            break; /* Exit on Ctrl+D */
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
+            break; /* Exit on EOF (Ctrl+D) */
         }
 
         /* Remove trailing newline */
         input[strcspn(input, "\n")] = '\0';
 
-        /* Check for the "exit" command */
-        if (strcmp(input, "exit") == 0) {
+        /* Check for exit command */
+        if (strcmp(input, "exit") == 0)
+        {
             should_run = 0;
-            continue; /* Skip the fork/exec process */
+            printf("Exiting the shell...\n");
         }
-
-        /* Fork a new process */
-        pid = fork();
-
-        if (pid < 0) {
-            perror("Fork failed");
-        } else if (pid == 0) {
-            /* Child process */
-            if (execlp(input, input, NULL) == -1) {
-                perror("Execution failed");
-                exit(EXIT_FAILURE);
-            }
-        } else {
-            /* Parent process */
-            int status;
-            waitpid(pid, &status, 0);
-            if (WIFEXITED(status)) {
-                if (WEXITSTATUS(status) != 0) {
-                    printf("Command exited with non-zero status %d\n", WEXITSTATUS(status));
-                }
-            } else {
-                printf("Command terminated abnormally\n");
-            }
+        else
+        {
+            /* Execute the command */
+            executeCommand(input);
         }
     }
 
-    return 0;
+    return (0);
+}
+
+/**
+ * executeCommand - Tokenizes and executes a command with arguments
+ * @input: The user input containing the command and optional arguments
+ */
+void executeCommand(char *input)
+{
+    char *args[MAX_INPUT_SIZE / 2 + 1];
+    int arg_count = 0;
+    char *token;
+
+    /* Tokenize the input */
+    token = strtok(input, " \n");
+    while (token != NULL)
+    {
+        args[arg_count] = token;
+        arg_count++;
+        token = strtok(NULL, " \n");
+    }
+    args[arg_count] = NULL;
+
+    /* Fork a new process */
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        perror("Fork failed");
+    }
+    else if (pid == 0)
+    {
+        /* Child process */
+        if (execvp(args[0], args) == -1)
+        {
+            perror("Execution failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        /* Parent process */
+        int status;
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status))
+        {
+            if (WEXITSTATUS(status) != 0)
+            {
+                printf("Command exited with non-zero status %d\n", WEXITSTATUS(status));
+            }
+        }
+        else
+        {
+            printf("Command terminated abnormally\n");
+        }
+    }
 }
 
